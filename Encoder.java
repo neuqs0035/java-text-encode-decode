9313921a1d36f8a66fb5545ccbd8816fe440d3a2
bhavya416 <152640221+bhavya416@users.noreply.github.com>
2023-12-06 21:05:15 +0530

    Encode Code Added

---
+++ Encoder.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Encoder {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter Text : ");
+        String message = scanner.next();
+        message += scanner.nextLine();
+
+        String encodedMessage = "";
+
+        for (int i = 0; i < message.length(); i++) {
+            int chrASCII = (int) message.charAt(i);
+            char encodedChar = (char)   (chrASCII  - 15);
+            encodedMessage += encodedChar;
+        }
+
+        encodedMessage += "e>";
+        System.out.println(encodedMessage);
+    }
+}
